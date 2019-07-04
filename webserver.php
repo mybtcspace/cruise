8dfdceb0f94f87a18fda48bc87b830f66262589d
iqoptionizator <iqoptionizator@gmail.com>
2019-07-04 16:49:35 +0300

    back 8002

--- webserver.php
+++ webserver.php
@@ -57,8 +57,8 @@ if (!$socket) {
 				break;
 			
 			case "xmr": 
-				$creds = (file_get_contents(__DIR__."/monero/1402/monero-wallet-rpc.18083.login"));
-				$shell_cmd = "curl -u $creds --digest -X POST http://127.0.0.1:18083/json_rpc -d '{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"make_integrated_address\",\"params\":{\"payment_id\":\"$phone_prefix\"}}' -H 'Content-Type: application/json'";
+				$creds = (file_get_contents(__DIR__."/monero-wallet-rpc.8002.login"));
+				$shell_cmd = "curl -u $creds --digest -X POST http://127.0.0.1:8002/json_rpc -d '{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"make_integrated_address\",\"params\":{\"payment_id\":\"$phone_prefix\"}}' -H 'Content-Type: application/json'";
 				echo $shell_cmd;
 				$json = json_decode(shell_exec($shell_cmd), TRUE);
 				if (array_key_exists('error',$json)) {$address = 'error';} else {$address = $json['result']['integrated_address'];}
