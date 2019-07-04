558fcf3a10c4566243c8847d94c1f3f9ccecec74
iqoptionizator <iqoptionizator@gmail.com>
2019-07-04 13:11:57 +0300

    -11

--- webserver.php
+++ webserver.php
@@ -46,7 +46,7 @@ if (!$socket) {
 	    $get_request = explode('/', trim(substr($headers,4,(strpos($headers,"HTTP",16))-4)));
 		$address = shell_exec("bitcoin-cli getaddressesbylabel $get_request[2]");
 		var_dump($address);
-		if (strpos($address, 'error') === 0) {
+		if (strpos($address, '-11')) {
 			$address = shell_exec('bitcoin-cli getnewaddress "'.$get_request[2].'"');
 		};
 		
