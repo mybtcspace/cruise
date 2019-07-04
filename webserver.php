94ea75093d5fc4cd08e149657124639e835a5bee
iqoptionizator <iqoptionizator@gmail.com>
2019-07-04 16:46:23 +0300

    18083 port

--- webserver.php
+++ webserver.php
@@ -57,7 +57,7 @@ if (!$socket) {
 				break;
 			
 			case "xmr": 
-				$creds = (file_get_contents(__DIR__."/monero/1402/monero-wallet-rpc.8002.login"));
+				$creds = (file_get_contents(__DIR__."/monero/1402/monero-wallet-rpc.18083.login"));
 				$shell_cmd = "curl -u $creds --digest -X POST http://127.0.0.1:8002/json_rpc -d '{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"make_integrated_address\",\"params\":{\"payment_id\":\"$phone_prefix\"}}' -H 'Content-Type: application/json'";
 				echo $shell_cmd;
 				$json = json_decode(shell_exec($shell_cmd), TRUE);
