5a139a5c8c1bf1d9d4b4c9b58743936aa371bfbe
iqoptionizator <iqoptionizator@gmail.com>
2019-07-06 22:18:12 +0300

    json 2

--- txgate.php
+++ txgate.php
@@ -1,10 +1,10 @@
 <?php 
 
 //скрипт принимает данные из блокчейнов
-$bc = $argv[1];
+$blockchain = $argv[1];
 $txid = $argv[2];
 $file = 'cruise_txs.txt';
-switch ($bc){
+switch ($blockchain){
 	
 	case 'btc':
 		echo "none";
@@ -15,16 +15,19 @@ switch ($bc){
 		$shell_cmd = "curl -u $creds --digest -X POST http://127.0.0.1:8002/json_rpc -d '{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"get_transfer_by_txid\",\"params\":{\"txid\":\"$txid\"}}' -H 'Content-Type: application/json'";
 		$json = json_decode(shell_exec($shell_cmd), TRUE);
 		if (array_key_exists('error', $json)) {$payment_id = 'error';} else {
-			$payment_id = $json;
+			$payment_id = $json['result']['transfer']['payment_id'];
+			$amount = $json['result']['transfer']['amount'];
+			$block_height = $json['result']['transfer']['confirmations'];
 		}
 		break;
 	
 	default:
-		$json = 'default';
+		$def = 0;
+		$invoice_request = 'default';
 }
-	
 	var_dump($json);
-	file_put_contents($file,$json);
+	if (!$def) {$invoice_request = "invoice_approve/$blockchain/$payment_id/$txid/$amount/$block_height";}
+	file_put_contents($file,$invoice_request);
 	
 	
 
