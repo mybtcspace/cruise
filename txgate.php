9efcea28a874c6ec914d008d2e7885718299fd78
iqoptionizator <iqoptionizator@gmail.com>
2019-07-06 22:29:33 +0300

    curl to server

--- txgate.php
+++ txgate.php
@@ -7,10 +7,12 @@ $file = 'cruise_txs.txt';
 switch ($blockchain){
 	
 	case 'btc':
-		echo "none";
+		$def = FALSE;
+		$invoice_request = 'default';
 		break;
 		
 	case 'xmr':
+		$def = TRUE;
 		$creds = file_get_contents(__DIR__."/monero-wallet-rpc.8002.login");
 		$shell_cmd = "curl -u $creds --digest -X POST http://127.0.0.1:8002/json_rpc -d '{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"get_transfer_by_txid\",\"params\":{\"txid\":\"$txid\"}}' -H 'Content-Type: application/json'";
 		$json = json_decode(shell_exec($shell_cmd), TRUE);
@@ -22,12 +24,20 @@ switch ($blockchain){
 		break;
 	
 	default:
-		$def = 0;
+		$def = FALSE;
 		$invoice_request = 'default';
 }
 	var_dump($json);
-	if (!$def) {$invoice_request = "invoice_approve/$blockchain/$payment_id/$txid/$amount/$block_height";}
-	file_put_contents($file,$invoice_request);
+	if (!$def) {
+		$uri = "http://ptback.mybtc.space/";
+		$invoice_request = "invoice_approve/$blockchain/$payment_id/$txid/$amount/$block_height";
+		$shell_cmd = "curl -X GET --data key $uri.$invoice_request";
+		$curl_data = shell_exec($shell_cmd);
+		var_dump($curl_data);
+		}
+	file_put_contents($file,"\r\n$invoice_request\r\n",FILE_APPEND);
+	
+	
 	
 	
 
