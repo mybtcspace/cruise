b76e38cbbe5b0670072f7613db7edd08990971df
iqoptionizator <iqoptionizator@gmail.com>
2023-06-30 00:22:05 +0300

    some fix Signed-off-by:iqoptionizator <iqoptionizator@gmail.com>

--- webserver.php
+++ webserver.php
@@ -118,7 +118,8 @@ if (!$socket) {
         $headers = '';
         while ($buffer = rtrim(fgets($connect))) {
             $headers .= $buffer;
-        }
+        }   
+            echo $get_request;
 	    $get_request = explode('/', trim(substr($headers,3,(strpos($headers,"HTTP",10))-4)));
 		$coin = preg_replace( "/[^a-zA-Z0-9\s]/", '', htmlentities(strip_tags($get_request[1])));
 		$phone_prefix = preg_replace( "/[^a-zA-Z0-9\s]/", '',htmlentities(strip_tags($get_request[2])));
