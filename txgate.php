1db4c615d8f63547f7a831fc774378f0b83304c9
iqoptionizator <iqoptionizator@gmail.com>
2023-07-02 04:15:21 +0300

    array fix Signed-off-by:iqoptionizator <iqoptionizator@gmail.com>

--- txgate.php
+++ txgate.php
@@ -101,7 +101,7 @@ switch ($blockchain){
 		$invoice_request = "invoices/approve/$blockchain/$payment_id/$txid/$amount/$block_height/$salt";
 		$shell_cmd = "curl -X GET --data key $uri.$invoice_request";
 		$curl_data = shell_exec($shell_cmd);
-		//var_dump($curl_data);
+		var_dump($curl_data);
 		}
 	file_put_contents($file,"\r\n$invoice_request\r\n",FILE_APPEND);
 	
